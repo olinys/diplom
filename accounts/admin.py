380c5dee782d06137dc559a360b35dbdee560ca0
Alina <melnichuk.alina12@gmail.com>
2025-04-09 19:36:35 +0300

    добавлены модели в админку джанго

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Product, Category, Order, User, UserProfile, Cart, PickupPoint, OrderProduct
+from .models import Product, Category, Order, UserProfile, Cart, PickupPoint, OrderProduct
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(UserProfile)
+admin.site.register(Cart)
+admin.site.register(PickupPoint)
+
 
